be96866641e0618560a4b34dfb36cdadfe7f208e
bena47 <bernard.njuguna@strathmore.edu>
2025-09-25 09:48:15 +0300

    added code into the products.js file

--- CSS/JS/products.js
+++ CSS/JS/products.js
@@ -1,5 +1,42 @@
 const products = [
     {
-
-    }
+id: 1,
+name: "Product 1",
+price: 1000,
+brand: "NIKE",
+image: "",
+description: "This is product 1",
+featured: true,
+colours: ["red", "blue", "green"]
+    },
+    {
+id: 2,
+name: "Product 2",
+price: 1000,
+brand: "ADIDAS",
+image: "",
+description: "This is product 2",
+featured: true,
+colours: ["red", "blue", "green"]
+    },
+    {
+id: 3,
+name: "Product 3",
+price: 1000,
+brand: "NIKE",
+image: "",
+description: "This is product 3",
+featured: true,
+colours: ["red", "blue", "green"]
+    },
+    {
+id: 4,
+name: "Product 4",
+price: 1000,
+brand: "NIKE",
+image: "",
+description: "This is product 4",
+featured: true,
+colours: ["red", "blue", "green"]
+    },
 ];
